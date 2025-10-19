733537d1162c13739c6acf4ed2217da3c73c60ab
thuyainsoe <thuyainsoe163361@gmail.com>
2025-10-19 16:34:52 +0700

    fix: first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full bg-white">
+      <div className="header-top bg-[#caddff] md-lg:hidden">
+        <div className="w-[85%] lg:w-[90%] mx-auto">
+          <div className="flex w-full justify-between items-center h-[50px] text-slate-500">
+            <ul className=""></ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
